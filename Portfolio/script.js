5ccccb98f94dfe764e29da139e07b05bee268763
vipasyan <vipasyan56@gmail.com>
2021-10-14 13:50:54 +0200

    Add java script file

---
+++ Portfolio/script.js
@@ -0,0 +1,8 @@
+let emailElement = document.querySelector('#email');
+let submitButton = document.querySelector('#submit-button');
+submitButton.addEventListener('click', function(e) 
+{
+	e.preventDefault();
+	console.log('clicked button')
+})
+
