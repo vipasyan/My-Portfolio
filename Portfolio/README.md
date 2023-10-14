f2b58cf75ef9b81efc5a06eeabc8a94f95b7236f
Vipasyan Telaprolu <vipasyan56@gmail.com>
2023-10-14 20:51:31 +0200

    Update files

---
+++ Portfolio/README.md
@@ -0,0 +1,2 @@
+# Main aim of this project is to create a website 
+ Learn new technologies by modifying website
--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,3 +1,10 @@
+
+// var username= prompt("Your name");
+// document.write("Hello "+ username);
+
+var myname = "Vipasyan Telaprolu";
+document.write("Made by "+ myname);
+
 let emailElement = document.querySelector('#email');
 let messageElement= document.querySelector('#message');
 let submitButton = document.querySelector('#submit-button');
@@ -20,3 +27,29 @@ submitButton.addEventListener('click', function(e)
 	
 })
 
+function addMyFavoriteBook(nameofBook) {
+	// 
+	if (!nameofBook.includes("win")){
+		favoriteBooks.push(nameofBook);
+		
+	}
+	
+  }
+  
+  function printFavoriteBooks(){
+	  console.log(`Favorite Books: ${favoriteBooks.length}`);
+	  for (let nameofBook of favoriteBooks) {
+		  console.log(nameofBook);
+	  }
+	  
+  }
+ var favoriteBooks = [];
+
+addMyFavoriteBook("Winners will never quit and quitters will never win");
+addMyFavoriteBook("You can win");
+addMyFavoriteBook("5 habits of winners");
+addMyFavoriteBook("Secret");
+addMyFavoriteBook("Power of your thought");
+addMyFavoriteBook("An autobiography of a yogi");
+
+printFavoriteBooks();
